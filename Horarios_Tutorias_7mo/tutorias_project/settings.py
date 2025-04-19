e6b4201b073d4996c30638309cb9aff6f827cf30
kelvinL55 <kaloaiza1@utpl.edu.ec>
2025-04-19 16:54:49 -0500

    Configure whitenoise for static files

--- Horarios_Tutorias_7mo/tutorias_project/settings.py
+++ Horarios_Tutorias_7mo/tutorias_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Directorio para archivos estáticos globales (opcional si solo usas los de las apps)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Almacenamiento recomendado para Whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
